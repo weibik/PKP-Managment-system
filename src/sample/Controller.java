95485f08cf500039716fe1dca9085161629d1955
weibik <j-szkola@wp.pl>
2021-05-12 17:53:18 +0200

    added current time and choiceboxes (staring and endstation)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,51 @@
 package sample;
 
-public class Controller {
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
 
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private ChoiceBox<String> start;
+    @FXML
+    private ChoiceBox<String> end;
+
+    @FXML
+    Label dateTime;
+
+    private String[] stations = {"Krakow, Wroclaw, Warszawa"};
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        start.getItems().addAll("Krakow", "Wroclaw", "Warszawa");
+        end.getItems().addAll("Krakow", "Wroclaw", "Warszawa");
+
+        initClock();
+    }
+
+    private void initClock() {
+
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            dateTime.setText(LocalDateTime.now().format(formatter));
+        }), new KeyFrame(Duration.seconds(1)));
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
+    }
 }
